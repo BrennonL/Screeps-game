a091e670c3a9ec6f5b21f9d2c3ce8de659be9f51
jafahulo <jafahulo@users.noreply.github.com>
2017-12-23 13:41:24 -0600

    Roughly add in structure for storage units

--- role.harvester.js
+++ role.harvester.js
@@ -11,11 +11,16 @@ var roleHarvester = {
         else {
             var targets = creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION ||
-                        structure.structureType == STRUCTURE_SPAWN ||
-                        structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
-                }
-            });
+                        for(struct in structure) {
+                            if(struct.structureType === STRUCTURE_STORAGE) {
+                                return Game.rooms['W5N8'].storage[RESOURCE_ENERGY] < Game.rooms['W5N8'].storage.storeCapacity;
+                            } else {
+                                return ((structure.structureType == STRUCTURE_EXTENSION ||
+                                structure.structureType == STRUCTURE_SPAWN ||
+                                structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity);
+                            }
+                        }
+            }});
             if(targets.length > 0) {
                 if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
